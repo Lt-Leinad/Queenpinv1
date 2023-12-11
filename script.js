5b12d21e97497534e10c138d7a3d33246f246407
Lt-Leinad <daniel.feddy0123@gmail.com>
2023-12-11 18:51:53 +0200

    done intersectional observer api

--- script.js
+++ script.js
@@ -16,3 +16,24 @@ window.addEventListener("load", function () {
 const xOutFunc = function () {
   document.querySelector(".contact-modal").classList.add("display-none");
 };
+
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
